49b829bfcbe79386eb97e1af8cec08d7948ad12a
Emma Forslund <emfo2102@student.miun.se>
2022-03-11 01:49:21 +0100

    Skapat admin.php

---
+++ admin.php
@@ -0,0 +1,71 @@
+<?php
+include('includes/config.php');
+//Kontroll för att se om användaren är inloggad. Olika navigeringar visas beroende på om användare är inloggad eller ej
+if (isset($_SESSION['email'])) {
+    include('includes/header-user.php');
+} else {
+    include('includes/header-public.php');
+}
+
+//Tar användaren till logga in-sidan om inloggningen misslyckades
+if (!isset($_SESSION['email'])) {
+    header("Location:login.php");
+}
+
+//Skapar ny instans av båda klasserna
+$user = new User();
+$post = new Post();
+$post_list = $post->getPosts();
+$username = $_SESSION['email'];
+
+
+//Loopar igenom listan med användare
+$usersList = $user->getUserInfo();
+foreach ($usersList as $user) {
+
+?>
+    <main>
+        <section class="admin">
+            <h2><?= $user['blog_name'] ?></h2>
+            <div id="logout"><a href="admin.php?logout">Logga ut</a></div>
+            <h3>Om <?= $user['blog_name'] ?></h3>
+            <div class="admin-flex">
+                <div id="left">
+                    <ul id="admin-ul">
+                        <li><strong>Bloggens namn:</strong><br> <?= $user['blog_name'] ?></li>
+                        <li><strong>Bloggarens namn:</strong><br> <?= $user['fname'] . " " . $user['ename'] ?>
+                        </li>
+                        <li><strong>Epost: </strong><br><?= $user['email'] ?>
+                        <li><strong>Blogg skapad: </strong><br><?= $user['created'] ?>
+                        </li>
+                    </ul>
+                </div>
+                <div id="right">
+                    <h2>Lagrade inlägg</h2>
+                    <?php
+                    $post = new Post();
+                    $post_list = $post->getPostByUser();
+
+                    //Kontroll om arrayen är tom 
+                    if (empty($post_list)) {
+                        echo "<p> Du har inte skapat några inlägg ännu.</p>";
+                    } else {
+                        //Loopar igenom listan med inlägg och skriver ut alla inlägg. 
+                        foreach ($post_list as $row) {
+                            echo " <article class='latest'>
+                           <h3>" . $row['title'] . "</h3>
+                           <p class='posted'>Postat: " . $row['created'] . " </p>
+                           <p>" . $row['content'] .
+                                "</article>";
+                        }
+                    } ?>
+                </div>
+            </div>
+        </section>
+    <?php
+
+}
+
+
+include('includes/footer.php');
+    ?>
