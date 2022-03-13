90f43400c34735f23f54f70fc12604ce73b0148c
Emma Forslund <emfo2102@student.miun.se>
2022-03-13 12:28:23 +0100

    Skapat funktionen för hamburger-menyn

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 "use strict";
+//Variabler
+let hamburger = document.getElementById("hamburger-icon");
+let navUl = document.getElementById("nav-ul");
+
+window.onload = init;
+
+function init() {
+
+  //Funktion för hamburger-menyn
+  hamburger.addEventListener("click", () => {
+    navUl.classList.toggle("show");
+  })
+}
 
 //Funktion för att göra så att submit-knappen inte fungerar när checkboxen inte är ibockad
 function disableSubmit(changed){
