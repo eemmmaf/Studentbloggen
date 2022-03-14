4103627c6de49b26459c86003677ca079cfd89f1
Emma Forslund <emfo2102@student.miun.se>
2022-03-14 23:18:00 +0100

    Lagt till HTML-koden för hamburger-meny

--- includes/header-public.php
+++ includes/header-public.php
@@ -1,4 +1,3 @@
-
 <!DOCTYPE html>
 <html lang="sv">
 
@@ -7,16 +6,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/styles.css">
+    <script src="https://kit.fontawesome.com/2090b52781.js" crossorigin="anonymous"></script>
     <title>Bloggportalen</title>
 </head>
+
 <body>
-<header>
+    <header>
         <h1><a href="index.php">Bloggportalen</a></h1>
         <nav class="desktop-nav">
             <ul>
-                <li><a href="about.php">Om oss</a></li>
-                <li><a href="register.php">Skapa blogg</a></li>
-                <li><a href="login.php">Logga in</a></li>
+                <li><a href="about.php">Om sidan   <i class="fa-solid fa-circle-info fa-2xs"></i></a></li>
+                <li><a href="register.php">Skapa blogg   <i class="fa-solid fa-user-plus fa-2xs"></i></a></li>
+                <li><a href="login.php">Logga in   <i class="fa-solid fa-right-to-bracket fa-2xs"></i></a></li>
+            </ul>
+        </nav>
+        <!--Hamburger-meny-->
+        <nav class="hamburger-menu">
+            <button class="hamburger-icon" id="hamburger-icon"><i class="fas fa-bars"></i></button>
+            <ul class="nav-ul" id="nav-ul">
+                <li><a href="about.php">Om sidan   <i class="fa-solid fa-circle-info fa-2xs"></i></a></li>
+                <li><a href="register.php">Skapa blogg   <i class="fa-solid fa-user-plus fa-2xs"></i></a></li>
+                <li><a href="login.php">Logga in   <i class="fa-solid fa-right-to-bracket fa-2xs"></i></a></li>
             </ul>
         </nav>
     </header>
