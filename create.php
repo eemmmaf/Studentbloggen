966b41366c46511abdbca892b7130592725c1f7d
Emma Forslund <emfo2102@student.miun.se>
2022-03-17 17:55:53 +0100

    Lagt till kommentarer och ändrat lite på utskrifter

--- create.php
+++ create.php
@@ -1,4 +1,5 @@
 <?php
+$page_title = "Hantera inlägg";
 include('includes/config.php');
 //Kontroll för att se om användaren är inloggad. Olika navigeringar visas beroende på om användare är inloggad eller ej
 if (isset($_SESSION['email'])) {
@@ -20,7 +21,7 @@ $username = $_SESSION['email'];
 if (isset($_GET['deleteid'])) {
     $deleteid = intval($_GET['deleteid']);
 
-    //Raderar
+    //Raderar inlägg
     if ($post->deletePost($deleteid)) {
         echo "<p id='raderat'> Inlägget har raderats </p>";
     } else {
@@ -39,31 +40,34 @@ if (isset($_POST['title'])) {
     $title = strip_tags($title);
     $content = $_POST['content'];
     $content = strip_tags($content);
+
+    
     //Felmeddelanden
     $success = true; //Variabel för när det postade är OK
     if (!$post->setTitle($title)) {
         $success = false;
 
-        echo "<p class='error-message'>Titel måste innehålla minst ett tecken</p>";
+        $errormessage= "<p class='error-create'>Titel måste innehålla minst ett tecken</p>";
     }
     if (!$post->setContent($content)) {
         $success = false;
 
-        echo "<p class='error-message'>Innehållet måste innehålla minst ett tecken</p>";
+       $errormessage = "<p class='error-create'>Innehållet måste innehålla minst ett tecken</p>";
     }
 
 
+    //Lägger till inlägget om input-fälten innehåller minst ett tecken
     if ($success) {
         if ($post->addPost($title, $content, $email)) {
-            $message = "<p id='lagrat-admin'> Inlägget har lagrats <i class='fa-solid fa-circle-check'></i> </p>";
+            $message = "<p class='stored'> Inlägget har lagrats <i class='fa-solid fa-circle-check'></i> </p>";
             //Nollställer default variablerna om inlägget postas
             $title = "";
             $content = "";
         } else {
-            $message =  "<p> Fel vid lagring </p>";
+            $message =  "<p class='stored'> Fel vid lagring </p>";
         }
     } else {
-        $message =  "<p class='not-stored'>Blogginlägg ej lagrat. Kontrollera innehållet och försök igen</p>";
+        $message =  "<p class='stored'>Blogginlägg ej lagrat. Kontrollera innehållet och försök igen</p>";
     }
 }
 
@@ -78,38 +82,47 @@ foreach ($usersList as $a) {
             <section class="admin">
                 <h2>Skapa inlägg</h2>
                 <?php
+                //Skriver ut meddelanden
                 if (isset($message)) {
                     echo $message;
+                }
+                if(isset($errormessage)){
+                    echo $errormessage;
                 }
                 ?>
 
                 <!--Formulär för att skapa inlägg-->
                 <form id="admin-form" method="post" action="create.php">
                     <div class="centered">
+                        <br>
                         <div><label for="title">Titel:</label><br></div>
                         <div><input type="text" id="title" name="title" value='' <?= $title; ?>><br><br></div>
                     </div>
                     <label for="content">Innehåll:</label><br>
                     <textarea id="content" name="content" rows="20"><?= $content; ?></textarea><br><br>
+                    <!--Byter ut textarea till CKEDITOR-->
                     <script>
                         CKEDITOR.replace('content');
                     </script>
                     <input type="submit" value="Skapa inlägg">
                 </form>
+                </section>
                 <section class="lagrad">
                     <h3>
                         Lagrade inlägg
                     </h3>
                     <?php
                     $post = new Post();
+                    //Anropar funktionen som hämtar den inloggade användarens inlägg
                     $post_list = $post->getPostByUser();
 
-                    //Loopar igenom listan med inlägg och skriver ut alla inlägg. 
+                    //Loopar igenom listan med inlägg och skriver ut alla inlägg om det finns några. Om det är tomt skrivs meddelande ut
                     if (empty($post_list)) {
                         echo "<p> Det finns inga lagrade inlägg </p>";
                     } else {
                         foreach ($post_list as $row) {
                     ?>
+                    <!--Utskrift-->
                             <article class="create-article">
                                 <h4><?= $row['title'] ?></h4>
                                 <p class="posted">Postat: <?= $row['created'] ?></p>
