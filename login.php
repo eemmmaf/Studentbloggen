6aadf77e0c666fbaf394dc86920ed410b47e0419
Emma Forslund <emfo2102@student.miun.se>
2022-03-19 16:01:03 +0100

    Lagt till programhuvud och tagit bort dubletter av kod

--- login.php
+++ login.php
@@ -1,41 +1,44 @@
 <?php
+/*
+ * @Author: Emma Forslund - emfo2102 
+ * @Date: 2022-03-17 19:56:39 
+ * @Last Modified by: Emma Forslund - emfo2102
+ * @Last Modified time: 2022-03-17 19:57:07
+ */
+
+
+$page_title = "Logga in";
 include('includes/config.php');
 //Kontroll för att se om användaren är inloggad. Olika navigeringar visas beroende på om användare är inloggad eller ej
-if(isset($_SESSION['email'])){
+if (isset($_SESSION['email'])) {
     include('includes/header-user.php');
-    }else{
-        include('includes/header-public.php');
-    }
+} else {
+    include('includes/header-public.php');
+}
 
-if(isset($_POST['email'])){
-    $email=$_POST['email'];
-    $password=$_POST['password'];
+//Sparar användarens mail och lösenord i variabler
+if (isset($_POST['email'])) {
+    $email = $_POST['email'];
+    $password = $_POST['password'];
 
     //Skapar instans av klassen User
     $login = new User();
-    if($login->logIn($email, $password)){
+    //Loggar in användaren om rätt användarnamn och lösenord anges
+    if ($login->logIn($email, $password)) {
         header('Location:admin.php');
-    }else{
+    } else {
         $message = "<p class='error-message'> Felaktig mailadress eller lösenord </p>";
     }
 }
 ?>
-<!DOCTYPE html>
-<html lang="sv">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles.css">
-    <title>Bloggportalen</title>
-</head>
 
 
+<!--Formulär för att logga in-->
 <form method="POST" action="login.php" id="login-form">
     <h2>Logga in</h2>
     <?php
-    if(isset($message)){
+    //Skriver ut felmeddelanden
+    if (isset($message)) {
         echo $message;
     }
     ?>
@@ -45,11 +48,16 @@ if(isset($_POST['email'])){
     <input type="password" name="password" id="password"><br><br>
     <input type="submit" value="Logga in">
     <div class="form-flex">
-    <div><h3>Har du inget konto?</h3></div>
-    <div><a href="register.php" id="member">Bli medlem</a></div>
+        <!--Länkar till register.php-->
+        <div>
+            <h3>Har du inget konto?</h3>
+        </div>
+        <div><a href="register.php" id="member">Bli medlem</a></div>
     </div>
-
 </form>
+<!--Slut på formulär-->
 
+<!--Inkluderar ej footern i denna fil-->
 </body>
+
 </html>
